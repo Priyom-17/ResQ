4fad4b5fc9d9606d1dc9f10acaaae5226bce2fc0
Priyom1 <soikatmirror23@gmail.com>
2024-02-02 22:28:29 +0600

    google map

--- lib/widgets/live_safe.dart
+++ lib/widgets/live_safe.dart
@@ -1,5 +1,6 @@
 import 'package:assignment_saleheen/widgets/home_widgets/live_safe/PoliceStationCard.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 import 'home_widgets/live_safe/BusStationCard.dart';
 import 'home_widgets/live_safe/HospitalCard.dart';
@@ -7,6 +8,17 @@ import 'home_widgets/live_safe/PharmacyCard.dart';
 
 class LiveSafe extends StatelessWidget {
   const LiveSafe({super.key});
+
+  static Future<void> openMap(String location) async {
+    String googleUrl='https://www.google.com/maps/search/$location';
+    final Uri _url = Uri.parse(googleUrl);
+    try {
+      await launchUrl(_url);
+    } catch (e) {
+  
+      
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
